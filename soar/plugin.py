be78ebefdca02880b84169bc69ed222a2d60ea82
Nyall Dawson <nyall.dawson@gmail.com>
2022-11-23 15:06:45 +1000

    Lint

--- soar/plugin.py
+++ soar/plugin.py
@@ -13,8 +13,22 @@ __copyright__ = 'Copyright 2022, North Road'
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
 
+from typing import Optional
 
-class SoarPlugin(object):
+from qgis.PyQt import sip
+from qgis.PyQt.QtCore import (
+    Qt,
+    QCoreApplication,
+    QEvent
+)
+from qgis.PyQt.QtWidgets import (
+    QAction
+)
+
+from .gui.browser_dock_widget import BrowserDockWidget
+
+
+class SoarPlugin:
     """
     Soar.earth plugin
     """
@@ -22,8 +36,49 @@ class SoarPlugin(object):
     def __init__(self, iface):
         self.iface = iface
 
+        self.dock: Optional[BrowserDockWidget] = None
+        self.browse_action: Optional[QAction] = None
+
+    # qgis plugin interface
+
+    # pylint: disable=missing-function-docstring
+
     def initGui(self):
-        pass
+        self.dock = BrowserDockWidget()
+        self.iface.addDockWidget(Qt.RightDockWidgetArea, self.dock)
+
+        self.browse_action = QAction(self.tr("Show Soar.earth Browser"), self.iface.mainWindow())
+        self.browse_action.setCheckable(True)
+        self.dock.setToggleVisibilityAction(self.browse_action)
+
+        self.dock.hide()
 
     def unload(self):
-        pass
+        if not sip.isdeleted(self.dock):
+            self.dock.cancel_active_requests()
+            self.iface.removeDockWidget(self.dock)
+            self.dock.deleteLater()
+        self.dock = None
+
+        if not sip.isdeleted(self.browse_action):
+            self.browse_action.deleteLater()
+        self.browse_action = None
+
+        QCoreApplication.sendPostedEvents(None, QEvent.DeferredDelete)
+
+    # pylint: enable=missing-function-docstring
+
+    @staticmethod
+    def tr(message):
+        """Get the translation for a string using Qt translation API.
+
+        We implement this ourselves since we do not inherit QObject.
+
+        :param message: String for translation.
+        :type message: str, QString
+
+        :returns: Translated version of message.
+        :rtype: QString
+        """
+        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
+        return QCoreApplication.translate('Soar', message)
