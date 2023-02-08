64e98a8e15ab1dd0132d1ad86fe99f5ac5f9a9ef
Nyall Dawson <nyall.dawson@gmail.com>
2023-02-08 16:35:22 +1000

    Show an acceptance dialog

--- soar/gui/__init__.py
+++ soar/gui/__init__.py
@@ -7,3 +7,4 @@ Contains GUI specific logic and classes
 from .gui_utils import GuiUtils
 from .thumbnail_manager import download_thumbnail
 from .map_export_dialog import MapExportDialog
+from .confirm_export_dialog import ConfirmExportDialog
