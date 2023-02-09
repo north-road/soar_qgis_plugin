86c9a2e21ff1cdc10f914de9d352f1abfc14f120
Nyall Dawson <nyall.dawson@gmail.com>
2023-02-09 14:23:11 +1000

    Style fixes

--- soar/gui/__init__.py
+++ soar/gui/__init__.py
@@ -4,8 +4,8 @@ GUI module
 Contains GUI specific logic and classes
 """
 
-from .gui_utils import GuiUtils
-from .thumbnail_manager import download_thumbnail
-from .map_export_dialog import MapExportDialog
 from .confirm_export_dialog import ConfirmExportDialog
+from .gui_utils import GuiUtils
 from .login_manager import LOGIN_MANAGER
+from .map_export_dialog import MapExportDialog
+from .thumbnail_manager import download_thumbnail
